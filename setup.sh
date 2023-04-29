759ab95da05434ff3f00b8e321871ab54283fdc3
Loup <90267658+Loup-Garou911XD@users.noreply.github.com>
2023-04-30 00:27:24 +0530

    Wednesday won't like this

--- setup.sh
+++ setup.sh
@@ -2,6 +2,16 @@
 
 log_file="/data/data/com.termux/files/home/bombsquad_setup.log"
 echo "beginning">$log_file
+raw_get_latest_link="https://raw.githubusercontent.com/Loup-Garou911XD/bombsquad_server-termux/main/get_latest_link.py"
+
+#colors
+clear="\033[0m"
+red="\033[31m"
+green="\033[32m"
+yellow="\033[33m"
+blue="\033[34m"
+purple="\033[35m"
+cyan="\033[36m"
 
 setup_storage(){
 yes|output=$(termux-setup-storage 2>>/dev/null) && 
@@ -10,36 +20,35 @@ output=$(ln -s /storage/emulated/0 /data/data/com.termux/files/usr/var/lib/proot
 
 #downloads and extracts the latest bombsquad server build for arm64
 get_latest_server_build(){
-raw_get_latest_link="https://raw.githubusercontent.com/Loup-Garou911XD/bombsquad_server-termux/main/get_latest_link.py"
 curl -so get_latest_link.py $raw_get_latest_link
 
-latest_server_build_link=$(proot-distro login ubuntu --termux-home -- python3 get_latest_link.py 2>/dev/null)
+latest_server_build_link=$(proot-distro login ubuntu --termux-home -- python3.10 get_latest_link.py)
 proot-distro login ubuntu -- curl -s $latest_server_build_link -o bs_server.tar.gz
 proot-distro login ubuntu -- tar -xzf bs_server.tar.gz
 }
 
+printf "${green}Installing proot-distro${clear}\n"
 $(apt-get update &>>$log_file)
 $(apt-get upgrade -y &>>$log_file)
 $(apt-get install proot-distro -y &>>$log_file)
 
 #installing proot-distro ubuntu
-output=$(proot-distro install ubuntu 2>&1)
-if [[ $output == *"is already installed"* ]]
-then
-    echo
-    echo "Ubuntu already installed, skipping!"
-else
-    echo $output
-fi
+printf "${green}Installing ubuntu in proot-distro${clear}\n"
+$(proot-distro install ubuntu &>$log_file)
+
+#updating ubuntu
+printf "${green}Updating ubuntu${clear}\n"
+output=$(proot-distro login ubuntu &>>$log_file -- apt-get update && apt-get upgrade -y)
 
 #setup to access storage in proot-distro
-read -p "This will give termux permission to access your storage and allow you to access it inside proot-distro.
-Do you want to Setup storage(y/n):" setup_storage_yn
+printf "${blue}This will give termux permission to access your storage and allow you to access it inside proot-distro.\nDo you want to Setup storage${clear}(y/n):"
+read setup_storage_yn
 case $setup_storage_yn in
     y|Y|yes|Yes|YES) setup_storage;
 esac
 
 #adding ubuntu login cmd to bash.bashrc 
+printf "${green}Adding login command to termux bashrc${clear}\n"
 login_cmd="proot-distro login ubuntu"
 bashrc="/data/data/com.termux/files/usr/etc/bash.bashrc"
 if grep -Fxq "$login_cmd" $bashrc
@@ -49,21 +58,22 @@ else
     echo $login_cmd>>$bashrc
 fi
 
-#updating ubuntu
-output=$(proot-distro login ubuntu &>>$log_file -- apt-get update && apt-get upgrade -y)
-
 #install python3.10?
-read -p "Install python3.10(y/n):" install_python_yn
+printf "${blue}Install python3.10${clear}(y/n):" 
+read install_python_yn
 case $install_python_yn in
-    y|Y|yes|Yes|YES) $(proot-distro login ubuntu -- apt-get install python3.10-dev -y &>>$log_file);
+    y|Y|yes|Yes|YES) printf "${green}Installing python3.10${clear}\n"; 
+		     $(proot-distro login ubuntu -- apt-get install python3.10-dev -y &>>$log_file);
 esac
-5
+
 #download latest server?
-read -p "Get latest bombsquad server(y/n):" get_latest_server_yn
-case $install_python_yn in
-    y|Y|yes|Yes|YES) get_latest_server_build;
+printf "${blue}Get latest bombsquad server${clear}(y/n):" 
+read get_latest_server_yn
+case $get_latest_server_yn in
+    y|Y|yes|Yes|YES) printf "${green}Downloading bombsquad server${clear}\n" ;
+		     get_latest_server_build;
 esac
 
 
-echo "Finished! Restart termux to login into ubuntu"
+printf "${cyan}Finished! Restart termux to login into ubuntu${clear}\n"
 
